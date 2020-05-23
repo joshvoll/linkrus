0b46f39c789329501a48fd1ab34009ffb4be6b98
Josue Rodriguez <joshvoll@yahoo.com>
2020-05-22 18:21:23 -0600

    adding the process of the link extractor

--- internal/crawler/link_extractor.go
+++ internal/crawler/link_extractor.go
@@ -1,8 +1,11 @@
 package crawler
 
 import (
+	"context"
 	"net/url"
 	"regexp"
+
+	"github.com/joshvoll/linkrus/internal/pipeline"
 )
 
 var (
@@ -12,6 +15,66 @@ var (
 	nofollowRegex  = regexp.MustCompile(`(?i)rel\s*?=\s*?"?nofollow"?`)
 )
 
+// linkExtractor model definition
+type linkExtractor struct {
+	netDetector PrivateNetworkDetector
+}
+
+// newLinkExtractor is the contructor function for the link extractor
+func newLinkExtractor(netDetector PrivateNetworkDetector) *linkExtractor {
+	return &linkExtractor{
+		netDetector: netDetector,
+	}
+}
+
+// Process is the encapsulation of the link extractor method
+// Search page content for a <base> tag and resolve it to an abs URL.
+// Find the unique set of links from the document, resolve them and
+// add them to the payload.
+func (le *linkExtractor) Process(ctx context.Context, p pipeline.Payload) (pipeline.Payload, error) {
+	payload := p.(*crawlerPayload)
+	relTo, err := url.Parse(payload.URL)
+	if err != nil {
+		return nil, err
+	}
+	content := payload.RawContent.String()
+	if baseMatch := baesHrefRegex.FindStringSubmatch(content); len(baseMatch) == 2 {
+		if base := resolveURL(relTo, ensureHasTrailingSlash(baseMatch[1])); base != nil {
+			relTo = base
+		}
+	}
+	seenMap := make(map[string]struct{})
+	for _, match := range findLinkRegex.FindAllStringSubmatch(content, -1) {
+		link := resolveURL(relTo, match[1])
+		if !le.retainLink(relTo.Hostname(), link) {
+			continue
+		}
+		link.Fragment = ""
+		linkStr := link.String()
+		if _, seen := seenMap[linkStr]; seen {
+			continue
+		}
+		if exclusionRegex.MatchString(linkStr) {
+			continue
+		}
+		seenMap[linkStr] = struct{}{}
+		if nofollowRegex.MatchString(match[0]) {
+			payload.NoFollowLinks = append(payload.NoFollowLinks, linkStr)
+		} else {
+			payload.Links = append(payload.Links, linkStr)
+		}
+	}
+	return payload, nil
+}
+
+// ensureHasTrailingSlash is check if the url has /
+func ensureHasTrailingSlash(s string) string {
+	if s[len(s)-1] != '/' {
+		return s + "/"
+	}
+	return s
+}
+
 // resolveURL expands target into an absolute URL using the following rules:
 // - targets starting with '//' are treated as absolute URLs that inherit the
 //   protocol from relTo.
@@ -20,7 +83,7 @@ var (
 // - all other targets are assumed to be relative to relTo.
 //
 // If the target URL cannot be parsed, an nil URL wil be returned
-func resolvedURL(relTo *url.URL, target string) *url.URL {
+func resolveURL(relTo *url.URL, target string) *url.URL {
 	tLen := len(target)
 	if tLen == 0 {
 		return nil
