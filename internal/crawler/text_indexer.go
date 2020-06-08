7a777e98266b98a88b156fb67210ed90ec5482a2
Josue Rodriguez <joshvoll@yahoo.com>
2020-06-08 17:36:49 -0600

    adding the crawler link processor

---
+++ internal/crawler/text_indexer.go
@@ -0,0 +1,37 @@
+package crawler
+
+import (
+	"context"
+	"time"
+
+	"github.com/joshvoll/linkrus/internal/pipeline"
+	"github.com/joshvoll/linkrus/internal/textindexer/index"
+)
+
+// textIndexer definition
+type textIndexer struct {
+	indexer index.Indexer
+}
+
+// newIndexer constructor function
+func newIndexer(indexer index.Indexer) *textIndexer {
+	return &textIndexer{
+		indexer: indexer,
+	}
+}
+
+// Process method implementation for the textIndexer type
+func (i *textIndexer) Process(ctx context.Context, p pipeline.Payload) (pipeline.Payload, error) {
+	payload := p.(*crawlerPayload)
+	doc := &index.Document{
+		LinkID:    payload.LinkID,
+		URL:       payload.URL,
+		Title:     payload.Title,
+		Content:   payload.TextContext,
+		IndexedAt: time.Now(),
+	}
+	if err := i.indexer.Index(doc); err != nil {
+		return nil, err
+	}
+	return p, nil
+}
